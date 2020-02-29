2ea14691dc2b02b6bd7de9404e7b7c4db05c993b
thiboswerts <thiboswerts@10.46.213.193>
2020-02-29 18:28:13 +0100

    API and test case, no implementation yet

---
+++ intlist/src/intlist/IntList.java
@@ -0,0 +1,30 @@
+package intlist;
+
+class Node {
+	int element;
+	Node next;
+}
+
+public class IntList {
+	
+	
+	
+	public int getSize() {
+		throw new RuntimeException("Not yet implemented");
+	}
+	
+	public int get(int index) {
+		throw new RuntimeException("Not yet implemented");
+	}
+	
+	public Intlist() {
+		throw new RuntimeException("Not yet implemented");
+		
+	}
+	
+	public void add(int element) {
+		throw new RuntimeExeption("Not yet implemented");
+		
+	}
+
+}
